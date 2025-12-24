2ed947ef6849009eb89750158d138fd91eb3c348
Kirill Bogachuk <bogachuk@sinergium.ru>
2025-12-24 21:27:09 +0300

    Additional exceptions in app class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from ya_schedule import YaSchedule
+from utils import catch_exception
 
 class Application:
     def __init__(self, api_key: str, api_key_geo: str) -> None:
@@ -10,15 +11,35 @@ class Application:
         print("1. Посмотреть расписание")
         print("3. Выход")
 
-    def __get_schedule(self) -> dict:
-        city_from = input("Введите город отправления: ")
-        city_to = input("Введите город назначения: ")
-
-        date = input("Введите дату отправления (YYYY-MM-DD): ")
-        transport_type = input("Введите тип транспорта: ")
+    @catch_exception()
+    def __input_transfers(self) -> Optional[str]:
         transfers_input = input("Включать маршруты с пересадками? (д/н): ")
         if transfers_input not in ("yes", "no", "y", "n", "д", "да", "н", "нет"):
             raise ValueError("Нужно ввести один из вариантов (д/н)")
+        return transfers_input
+
+    @catch_exception()
+    def __input_city(self, city_type: str) -> Optional[str]:
+        city = input(f"Введите город {city_type}: ")
+        if not city:
+            raise ValueError(f"Необходимо ввести город {city_type}")
+        return city
+
+    def __get_schedule(self) -> Optional[dict]:
+        city_from = self.__input_city(city_type="отправления")
+        while not city_from:
+            city_from = self.__input_city("отправления")
+
+        city_to = self.__input_city(city_type="назначения")
+        while not city_to:
+            city_to = self.__input_city("назначения")
+
+        date = input("Введите дату отправления (YYYY-MM-DD): ")
+        transport_type = input("Введите тип транспорта: ")
+
+        transfers_input = self.__input_transfers()
+        while not transfers_input:
+            transfers_input = self.__input_transfers()
 
         transfers = False
         if transfers_input == "д" \
